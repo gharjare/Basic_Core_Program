f5294a26a021490692d7e2f06bf93f5b5d28124c
gharjare <gharjarechetan@gmail.com>
2023-01-28 14:45:22 +0530

    [chetan gharjare] here swapping two number without third number program completed

--- Program.cs
+++ Program.cs
@@ -5,14 +5,14 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to basic core program");
-            int divident = 50, divisor = 8;
-            int quotient = divident / divisor;
-            int remainder = divident % divisor;
-            Console.WriteLine("Divident : {0} Divisor : {1}", divident, divisor);
-            Console.WriteLine("Quotient :" + quotient);
-            Console.WriteLine("Remainder : " + remainder);
-
+            int a = 10, b = 20, temp;
+            Console.WriteLine("Before swapping value of a={0},b={1}", a, b);
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("After the swapping value of a={0},b={1}", a, b);
         }
     }
+    
     
 }
